c2526b5c47e224c7204d07a0779e4cb845d6e025
Kevin M. Smith <ksmith.nop@gmail.com>
2018-09-05 12:53:52 -0400

    Automatically setup vim plugins.

--- setup.sh
+++ setup.sh
@@ -4,3 +4,25 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 ln -s $(realpath $DIR/.vimrc) ~/.vimrc
 ln -s $(realpath $DIR/.tmux.conf) ~/.tmux.conf
+
+if [ ! -d "~/.vim/autoload/pathogen.vim" ]; then
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+if [ ! -d "~/.vim/bundle/ctrlp.vim" ]; then
+	git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+fi
+
+if [ ! -d "~/.vim/bundle/vim-sneak" ]; then
+	git clone git://github.com/justinmk/vim-sneak.git ~/.vim/bundle/vim-sneak
+fi
+
+if [ ! -d "~/.vim/bundle/taby.vim" ]; then
+	git clone git://github.com/kevinms/taby.vim.git ~/.vim/bundle/taby.vim
+fi
+
+if [ ! -d "~/.vim/bundle/vim-eunuch" ]; then
+	git clone https://github.com/tpope/vim-eunuch.git ~/.vim/bundle/vim-eunuch
+fi
+
