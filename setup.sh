cd9618bfe4270a0b91499b41b2bb97fb35cb46bc
Kevin M. Smith <ksmith.nop@gmail.com>
2018-09-05 21:17:17 -0400

    Fixed issue with clonling vim-sneak.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,32 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+plugin=~/.vim/autoload/pathogen.vim
+if [ ! -d $plugin ]; then
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso $plugin https://tpo.pe/pathogen.vim
+fi
+
+plugin=~/.vim/bundle/ctrlp.vim
+if [ ! -d $plugin ]; then
+	git clone https://github.com/kien/ctrlp.vim.git $plugin
+fi
+
+plugin=~/.vim/bundle/vim-sneak
+if [ ! -d $plugin ]; then
+	git clone git://github.com/justinmk/vim-sneak.git $plugin
+fi
+
+plugin=~/.vim/bundle/taby.vim
+if [ ! -d $plugin ]; then
+	git clone git://github.com/kevinms/taby.vim.git $plugin
+fi
+
+plugin=~/.vim/bundle/vim-eunuch
+if [ ! -d $plugin ]; then
+	git clone https://github.com/tpope/vim-eunuch.git $plugin
+fi
+
 must_not_exist() {
 	if [ -f $1 ]; then
 		echo "$1 already exists?!"
@@ -16,30 +42,4 @@ ln -s $(realpath $DIR/.vimrc) ~/.vimrc
 ln -s $(realpath $DIR/.tmux.conf) ~/.tmux.conf
 
 ls -lash ~/.vimrc ~/.tmux.conf
-
-plugin=~/.vim/autoload/pathogen.vim
-if [ ! -d $plugin ]; then
-	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso $plugin https://tpo.pe/pathogen.vim
-fi
-
-plugin=~/.vim/bundle/ctrlp.vim
-if [ ! -d $plugin ]; then
-	git clone https://github.com/kien/ctrlp.vim.git $plugin
-fi
-
-plugin=~/.vim/bundle/ctrlp.vim
-if [ ! -d $plugin ]; then
-	git clone git://github.com/justinmk/vim-sneak.git $plugin
-fi
-
-plugin=~/.vim/bundle/taby.vim
-if [ ! -d $plugin ]; then
-	git clone git://github.com/kevinms/taby.vim.git $plugin
-fi
-
-plugin=~/.vim/bundle/vim-eunuch
-if [ ! -d $plugin ]; then
-	git clone https://github.com/tpope/vim-eunuch.git $plugin
-fi
 
